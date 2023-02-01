0b77f9d9f3ea8b64e1bfeef670e2a969bb5ae216
Riley Lee <rileylyw@gmail.com>
2023-02-01 23:31:31 +0800

    initial

---
+++ App.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  const [goal, setGoal] = useState('')
+
+  function goalInputHandler(text) {
+    setGoal(text)
+  }
+
+  function addGoldHandler() {
+    console.log(goal)
+  }
+
+
+
+  return (
+    <View style={styles.appContainer}>
+      {/* button to add goals */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your goals!'
+          onChangeText={goalInputHandler} />
+        <Button
+          title='Add Goal'
+          onPress={addGoldHandler} />
+      </View>
+
+      {/* list of goals */}
+      <View style={styles.goalsContainer}>
+        <Text>List of goals:</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5
+  }
+});
