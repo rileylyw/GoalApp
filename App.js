bf86f14158b6b3584e05ee4aa6412be00bc28d7e
Riley Lee <rileylyw@gmail.com>
2023-02-02 23:56:46 +0800

    list state added

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [goal, setGoal] = useState('')
+  const [goals, setGoals] = useState([])
 
   function goalInputHandler(text) {
     setGoal(text)
   }
 
   function addGoldHandler() {
-    console.log(goal)
+    setGoals(currentGoals => [...currentGoals, goal])
   }
 
 
@@ -29,7 +30,12 @@ export default function App() {
 
       {/* list of goals */}
       <View style={styles.goalsContainer}>
-        <Text>List of goals:</Text>
+        <Text style={styles.listOfGoals}>List of goals:</Text>
+        {goals.map((goal) => (
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -58,6 +64,24 @@ const styles = StyleSheet.create({
     padding: 8
   },
   goalsContainer: {
-    flex: 5
+    flex: 5,
+    marginHorizontal: 15
+  },
+  listOfGoals: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 15
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    borderWidth: 3,
+    borderColor: '#5e0acc',
+  },
+  goalText: {
+    color: '#5e0acc',
+    paddingHorizontal: 15,
+    fontWeight: 'bold'
   }
 });
